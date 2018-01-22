7a058393dd61b1ff46de3981eb8115b00d769cbd
y_hui <3043909700@qq.com>
2018-01-22 10:19:39 +0800

    1、接口文档上传； 2、设置里拨打电话处理

---
+++ app/src/main/java/com/wjc/parttime/mvp/personal_center/PersonalInfoActivity.java
@@ -0,0 +1,22 @@
+package com.wjc.parttime.mvp.personal_center;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.wjc.parttime.R;
+
+public class PersonalInfoActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/wjc/parttime/setting/PersonalSettingActivity.java
+++ app/src/main/java/com/wjc/parttime/setting/PersonalSettingActivity.java
@@ -1,13 +1,20 @@
 package com.wjc.parttime.setting;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.mukesh.permissions.AppPermissions;
 import com.wjc.parttime.R;
 import com.wjc.parttime.util.CommonDialogUtil;
 import com.wjc.parttime.util.LogUtil;
@@ -37,9 +44,19 @@ public class PersonalSettingActivity extends Activity implements View.OnClickLis
 
     TextView title;
 
+    //客服热线
+    private String phoneNumber = "18250154818";
+
     private String TAG = "PersonalSettingActivity";
 
     CommonDialogUtil dialog;
+
+    String[] allPermissions = {
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CALL_PHONE
+    };
+    public static final int READ_EXTERNAL_STORAGES = 1;
+    public static final int WRITE_EXTERNAL_STORAGES = 2;
+    public static final int CALL_PHONE = 3;
 
 
     @Override
@@ -88,7 +105,13 @@ public class PersonalSettingActivity extends Activity implements View.OnClickLis
                 break;
             case R.id.person_setting_rl_connect_us:
                 //联系我们
-                LogUtil.e(TAG, "点击");
+                AppPermissions runtimePermission = new AppPermissions(this);
+                if (!runtimePermission.hasPermission(Manifest.permission.CALL_PHONE)) {
+                    runtimePermission.requestPermission(allPermissions, CALL_PHONE);
+                    return;
+                } else {
+                    callPhone(phoneNumber);
+                }
                 break;
             case R.id.person_setting_rl_shared:
                 //分享
@@ -96,4 +119,52 @@ public class PersonalSettingActivity extends Activity implements View.OnClickLis
                 break;
         }
     }
+
+    /*
+    * 拨打电话
+    * */
+    private void callPhone(final String phoneNumber) {
+        dialog = new CommonDialogUtil(PersonalSettingActivity.this, R.style.dialog, "呼叫 " + phoneNumber, "确定","取消", new CommonDialogUtil.OnListener() {
+            @Override
+            public void onCancelclick() {
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onConfirmClick() {
+                Intent call = new Intent(Intent.ACTION_CALL);
+                call.setData(Uri.parse("tel:" + phoneNumber));
+                if (ActivityCompat.checkSelfPermission(PersonalSettingActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(call);
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGES:
+            case WRITE_EXTERNAL_STORAGES:
+                break;
+            case CALL_PHONE:
+                callPhone(phoneNumber);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
 }
